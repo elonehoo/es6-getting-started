4f225fec9560522a3c992a448607ff27e03b6d7a
xiaoxunyao <huchengyea@163.com>
2022-04-23 21:05:05 +0800

    chore: init

---
+++ .vitepress/config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang:"zh-CN",
+  title:"ECMAScript 6",
+  description:"ES6 Getting Started Tutorial",
+  themeConfig:{
+    sidebar:[
+      {
+        text:"简介",
+        link:"index"
+      }
+    ]
+  }
+})
