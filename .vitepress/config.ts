6545d53044751667df9640ce3caf278c138cefc0
xiaoxunyao <huchengyea@163.com>
2022-04-23 21:28:36 +0800

    fix: update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base:"/",
   lang:"zh-CN",
   title:"ECMAScript 6",
   description:"ES6 Getting Started Tutorial",
+  head:[
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/logo.svg'
+      }
+    ]
+  ],
   themeConfig:{
+    logo:"logo.svg",
     sidebar:[
       {
         text:"简介",
         link:"index"
+      },
+      {
+        text: "let 和 const 命令",
+        link: "docs/let"
       }
     ]
   }
